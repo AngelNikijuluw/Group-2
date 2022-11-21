0a96b398071f6d41bfcc09953314f6b41d1bd7be
Fahri Efendi <fahriefendi0795@gmail.com>
2022-11-22 00:02:07 +0700

    add img , login page done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,43 +1,63 @@
 import React from "react";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { Container } from "react-bootstrap";
+import logo from "../../Assets/logo.png";
+import gambar from "../../Assets/gambar.png";
 
 export default function Login() {
   return (
-    <div class="login-form">
-      <form>
-        <h1>Login</h1>
-        <div class="content">
-          <div class="input-field">
-            <input type="email" placeholder="Email" autocomplete="nope"></input>
-          </div>
-          <div class="input-field">
-            <input
-              type="password"
-              placeholder="Password"
-              autocomplete="new-password"
-            ></input>
-          </div>
-          <a href="." class="link">
-            Forgot Your Password?
-          </a>
-        </div>
-        <div class="action">
-          <button>Sign in</button>
-        </div>
-        <div class="google">
-          <button>
-            <a>
-              <AiFillGoogleCircle />
+    <Container fluid className="home-login" id="Login">
+      <div
+        style={{ float: "left", marginTop: "200px", marginLeft: "400px" }}
+        className="img-login"
+      >
+        <img src={logo}></img>
+      </div>
+      <div style={{ float: "left", marginTop: "300px", marginLeft: "-420px" }}>
+        <img src={gambar}></img>
+      </div>
+      <div class="login-form">
+        <form>
+          <h1>Login</h1>
+          <div class="content">
+            <div class="input-field">
+              <input
+                type="email"
+                placeholder="Email"
+                autocomplete="nope"
+                required
+              ></input>
+            </div>
+            <div class="input-field">
+              <input
+                type="password"
+                placeholder="Password"
+                autocomplete="new-password"
+                required
+              ></input>
+            </div>
+            <a href="." class="link">
+              Forgot Your Password?
             </a>
-            Log in with Google
-          </button>
-        </div>
-        <div>
-          <p>
-            Don’t have an account yet? <a href="/SignUp">Sign up</a>.
-          </p>
-        </div>
-      </form>
-    </div>
+          </div>
+          <div class="action">
+            <button>Sign in</button>
+          </div>
+          <div class="google">
+            <button>
+              <a>
+                <AiFillGoogleCircle />
+              </a>
+              Log in with Google
+            </button>
+          </div>
+          <div>
+            <p>
+              Don’t have an account yet? <a href="/SignUp">Sign up</a>.
+            </p>
+          </div>
+        </form>
+      </div>
+    </Container>
   );
 }
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,35 +1,60 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import logo from "../../Assets/logo.png";
+import gambar from "../../Assets/gambar.png";
 
 export default function Signup() {
   return (
-    <div class="login-form">
-      <form>
-        <h1>Sign Up</h1>
-        <div class="content">
-          <div class="input-field">
-            <input type="nama" placeholder="Nama" autocomplete="nope"></input>
+    <Container fluid className="home-signup" id="signup">
+      <div
+        style={{ float: "left", marginTop: "200px", marginLeft: "400px" }}
+        className="img-login"
+      >
+        <img src={logo}></img>
+      </div>
+      <div style={{ float: "left", marginTop: "300px", marginLeft: "-420px" }}>
+        <img src={gambar}></img>
+      </div>
+      <div class="login-form">
+        <form>
+          <h1>Sign Up</h1>
+          <div class="content">
+            <div class="input-field">
+              <input
+                type="nama"
+                placeholder="Nama"
+                autocomplete="nope"
+                required
+              ></input>
+            </div>
+            <div class="input-field">
+              <input
+                type="email"
+                placeholder="Email"
+                autocomplete="nope"
+                required
+              ></input>
+            </div>
+            <div class="input-field">
+              <input
+                type="password"
+                placeholder="Password"
+                autocomplete="new-password"
+                required
+              ></input>
+            </div>
+            <a href="#" class="link">
+              Forgot Your Password?
+            </a>
           </div>
-          <div class="input-field">
-            <input type="email" placeholder="Email" autocomplete="nope"></input>
+          <div class="action">
+            <button>Sign in</button>
           </div>
-          <div class="input-field">
-            <input
-              type="password"
-              placeholder="Password"
-              autocomplete="new-password"
-            ></input>
+          <div class="google">
+            <button>Sign in Google</button>
           </div>
-          <a href="#" class="link">
-            Forgot Your Password?
-          </a>
-        </div>
-        <div class="action">
-          <button>Sign in</button>
-        </div>
-        <div class="google">
-          <button>Sign in Google</button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </Container>
   );
 }
